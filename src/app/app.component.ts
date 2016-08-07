7d77551799c63a51e7e1ef538ad12298f8061d13
Dyam Marcano <dyam.marcano@gmail.com>
2016-08-07 08:13:06 -0400

    fixes #04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   template: require('./app.component.html'),
-  styles: [
-  	require('./app.component.scss')
-  ],
+  styles: [require('./app.component.scss')],
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  message = 'app works!';
+  socket: any = null;
+  chatinp: string = '';
+
+  constructor() {
+    this.socket = io('http://localhost:9800');
+    this.socket.on('message', (msg: string) => {
+      console.log(msg);
+    })
+  };
+
   hello = 'Hello World';
+
+  send(msg: string) {
+    this.socket.emit('message', msg);
+  }
 }
